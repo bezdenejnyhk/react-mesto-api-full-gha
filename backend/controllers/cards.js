f896674fd5c7dc8bdc303924f64554a97924fb96
Яковлева Ксения <bezdenejnyhk@yandex.ru>
2023-06-04 19:22:38 +0300

    kdkflfmm

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -20,6 +20,7 @@ const createCard = (req, res, next) => {
 
 const getCards = (req, res, next) => {
   Card.find({})
+    .populate(['owner', 'likes'])
     .then((cards) => res.send(cards))
     .catch(next);
 };
