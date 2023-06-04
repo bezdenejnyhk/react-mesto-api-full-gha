9c57e73d1f3d06fbe0942b69e740bf22041640eb
Яковлева Ксения <bezdenejnyhk@yandex.ru>
2023-06-04 10:31:11 +0300

    settingt

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -28,7 +28,7 @@ export const checkToken = (token) => {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
+      authorization: `Bearer ${token}`,
     },
   }).then(checkResponse);
 };
